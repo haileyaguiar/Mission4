8d9f839cbe1af1174f3e5f751af6ff7180b7a909
HaileySanders82 <103207122+HaileySanders82@users.noreply.github.com>
2024-01-31 16:30:13 -0700

    lots of code yum yum

--- tools.cs
+++ tools.cs
@@ -10,11 +10,51 @@ namespace Mission4
 {
     internal class tools
     {
+        private char[,] board;
+
+        public void SetBoard(char[,] receivedBoard)
+        {
+            board = receivedBoard;
+        }
+
+        public void PrintBoard()
+        {
+            Console.WriteLine("Current Board:");
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write(board[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+        }
+
+        public char CheckForWinner()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                // Check rows
+                if (board[i, 0] != ' ' && board[i, 0] == board[i, 1] && board[i, 1] == board[i, 2])
+                    return board[i, 0];
+
+                // Check columns
+                if (board[0, i] != ' ' && board[0, i] == board[1, i] && board[1, i] == board[2, i])
+                    return board[0, i];
+            }
+
+            // Check diagonals
+            if (board[0, 0] != ' ' && board[0, 0] == board[1, 1] && board[1, 1] == board[2, 2])
+                return board[0, 0];
+
+            if (board[0, 2] != ' ' && board[0, 2] == board[1, 1] && board[1, 1] == board[2, 0])
+                return board[0, 2];
+
+            // If no winner, return C for Cat's game:
+            return 'C';
+        }
     }
 }
-
-//The supporting class (with whichever name you choose) will:
-//• Receive the “board” array from the driver class
-//• Contain a method that prints the board based on the information passed to it
-//• Contain a method that receives the game board array as input and returns if there is a
-//winner and who it was
