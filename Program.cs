1ccb279c6a26163d22f8c6be71d7b23a7a2d609b
benmorey <dbmoreyjr@gmail.com>
2024-02-01 20:17:29 -0700

    one last one...

--- Program.cs
+++ Program.cs
@@ -1,32 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Mission4;
 using System.Runtime.InteropServices;
+
+//Mission 4 Assignment
+//Group 003 - 15
+// Ben Morey, Hailey Aguiar, Johnny Fietkau, Kate Hendricksen
+
+
+//Starts the game
 Console.WriteLine("⭕ Welcome to Tic-Tac-Toe! ❌\n");
-
-
-//The “Driver” class (the Program.cs class with the main method where the program begins)
-//will:
-//• Welcome the user to the game
-//• Create a game board array to store the players’ choices
-//• Ask each player in turn for their choice and update the game board array
-//• Print the board by calling the method in the supporting class
-//• Check for a winner by calling the method in the supporting class, and notify the players
-//when a win has occurred and which player won the game
-
-
-
-//The supporting class (with whichever name you choose) will:
-//• Receive the “board” array from the driver class
-//• Contain a method that prints the board based on the information passed to it
-//• Contain a method that receives the game board array as input and returns if there is a
-//winner and who it was
-
 
 Console.WriteLine("What is the first players name?");
 string userName1 = Console.ReadLine();
 Console.WriteLine("What is the second players name?");
 string userName2 = Console.ReadLine();
-//• Welcome the users to the game
+
+// Welcomes the users to the game
 Console.WriteLine($"Hello {userName1} and {userName2}!");
 
 
@@ -134,6 +123,8 @@ while (1 != 0)
         Console.WriteLine(userName2 + " has won!");
         break;
     }
+
+    //If it's a tie
     if (tls.CheckForWinner(gameboard) == 'C')
     {
         Console.WriteLine("It's a tie!");
